362e706572aa4339bad1a345f5eda3451dea3be3
kojimayu <bousouko777@gmail.com>
2019-04-09 17:02:52 +0900

    06end

--- .history/js/main_20190409165858.js
+++ .history/js/main_20190409165858.js
@@ -6,5 +6,3 @@ console.log(10 * 3);
 console.log(10 / 3);
 console.log(10 % 3);
 console.log(10 ** 3);
-
-console.log((2 + 10) * 3);
--- .history/js/main_20190409170141.js
+++ .history/js/main_20190409170141.js
@@ -7,4 +7,4 @@ console.log(10 / 3);
 console.log(10 % 3);
 console.log(10 ** 3);
 
-console.log((2 + 10) * 3);
+console.log(2 + 10 * 3);
