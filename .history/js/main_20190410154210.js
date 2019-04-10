bfadffacbe05b805816ef4c96006627588005e12
kojimayu <bousouko777@gmail.com>
2019-04-10 15:45:37 +0900

    16end

---
+++ .history/js/main_20190410154210.js
@@ -0,0 +1,6 @@
+'use strict'
+// for
+
+for (let i = 1; i <= 10; i++) {
+    console.log('Hello');
+}
--- .history/js/main_20190410154311.js
+++ .history/js/main_20190410154311.js
@@ -3,6 +3,5 @@
 
 for (let i = 1; i <= 10; i++) {
     // console.log('Hello');
-    // console.log(i + 'hello');
-    console.log(`${i} hello`);
+    console.log(i + 'hello');
 }
